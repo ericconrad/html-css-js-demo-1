7e7351d6bb4bdd05053d86731b5968994a45de11
Eric Conrad <3170279+ericconrad@users.noreply.github.com>
2026-04-08 19:25:20 -0400

    Added SFX

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 // alert("hello world!");
+function banana() {
+  // alert("Nav is toggled");
+  document.getElementById("main-nav").classList.toggle("open");
+}
+
+window.addEventListener('click', () => {
+    new Audio('audio/813182__ldsqui__tiny-dink.wav').play();
+});
